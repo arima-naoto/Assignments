6bb3d19ecd8bd5cb860fd55f7aab88fb9db387c0
arima-naoto <k023g1010@g.neec.ac.jp>
2024-09-26 00:15:32 +0900

    円周上を回る円を実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include "math.h"
 
 const char kWindowTitle[] = "GC1C_02_アリマ_ナオト";
+
+struct Vector2 {
+	float x;
+	float y;
+};
+
+
+
+void RotateCircle(float& theta, Vector2& position, const Vector2& circle, const Vector2& targetRadius) {
+
+	theta += 1.0f / 60.0f * float(M_PI);
+
+	position.x = circle.x + (targetRadius.x * cosf(theta));
+	position.y = circle.y + (targetRadius.y * sinf(theta));
+
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +29,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	float theta = 0;
+
+	Vector2 position = { 0,0 };
+	Vector2 circlePos = { 400,400 };
+	Vector2 targetRadius = { 200,200 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +49,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		RotateCircle(theta, position, circlePos, targetRadius);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -33,6 +59,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 	
+		Novice::DrawEllipse((int)position.x, (int)position.y, 50, 50, theta, RED, kFillModeSolid);
+
 		///
 		/// ↑描画処理ここまで
 		///
