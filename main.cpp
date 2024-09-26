aa6a999fec182d4dbb9b6b22c63ceff65f0a0d9e
arima-naoto <k023g1010@g.neec.ac.jp>
2024-09-26 21:04:09 +0900

    カーソルで出現させた円の中心を白い円が回る処理を実装

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 const char kWindowTitle[] = "GC1C_02_アリマ_ナオト";
 
@@ -11,6 +13,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	int mouseX = 0;
+	int mouseY = 0;
+
+	float positionX = 400;
+	float positionY = 400;
+
+	int radius = 20;
+
+	float theta = 0;
+
+	int isAdvent = FALSE;
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +40,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		if (!isAdvent) {
+			Novice::GetMousePosition(&mouseX, &mouseY);
+		}
+		if (Novice::IsTriggerMouse(0)) {
+			isAdvent = TRUE;
+		}
+
+		if (isAdvent) {
+
+			theta -= 1.0f / 60.0f * float(M_PI);
+
+			positionX = mouseX - (200 * cosf(theta));
+			positionY = mouseY + (200 * sinf(theta));
+
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -33,6 +64,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 	
+
+		Novice::DrawEllipse((int)positionX, (int)positionY, radius, radius, theta, WHITE, kFillModeSolid);
+
+		if (isAdvent) {
+			Novice::DrawEllipse(mouseX, mouseY, 10, 10, 0.0f, RED, kFillModeSolid);
+		}
+
 		///
 		/// ↑描画処理ここまで
 		///
