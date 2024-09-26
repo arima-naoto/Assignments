977fb74431d034a2221d2b5e548011987560fe33
arima-naoto <k023g1010@g.neec.ac.jp>
2024-09-26 13:18:09 +0900

    一秒間ごとにいったりきたりする処理を実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include "math.h"
+
+#include "imgui.h"
 
 const char kWindowTitle[] = "GC1C_02_アリマ_ナオト";
+
+bool isCollision(int mouseX, int mouseY, float positionX, float positionY,int radius) {
+	
+	Novice::GetMousePosition(&mouseX, &mouseY);
+
+	float a = positionX - (float)mouseX;
+	float b = positionY - (float)mouseY;
+
+	float c = sqrtf(powf(a, 2) + powf(b, 2));
+
+	if (c <= (float)radius) {
+		return true;
+	}
+
+	return false;
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +31,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	int timer = 0;
+
+	float positionX = 500;
+	float positionY = 500;
+	int radius = 30;
+	float theta = 0;
+	unsigned int color = WHITE;
+
+	FillMode fillMode = kFillModeSolid;
+
+	bool inverse = false;
+
+	int mouseX = 0;
+	int mouseY = 0;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +60,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		if (++timer >= 55) {
+			inverse = true;
+
+			if (timer >= 110) {
+				inverse = false;
+				timer = 0;
+			}
+
+		}
+
+		if (!inverse) {
+			theta -= (1.0f / 256.0f) * float(M_PI);
+		}
+		else {
+			theta += (1.0f / 256.0f) * float(M_PI);
+		}
+
+		float newPositionX = positionX * cosf(theta) - positionY * sinf(theta);
+		float newPositionY = positionY * cosf(theta) + positionX * sinf(theta);
+
+		if (isCollision(mouseX, mouseY, newPositionX, newPositionY, radius)) {
+			color = RED;
+		}
+		else {
+			color = WHITE;
+		}
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -33,6 +95,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 	
+		Novice::DrawEllipse(int(newPositionX), int(newPositionY), radius, radius, theta, color, fillMode);
+
 		///
 		/// ↑描画処理ここまで
 		///
