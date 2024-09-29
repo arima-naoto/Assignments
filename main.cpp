9374a6fe86459a7f1e427ebad683754ae679f3cb
arima-naoto <k023g1010@g.neec.ac.jp>
2024-09-29 22:12:24 +0900

    ファイヤーボールを実装し、衝突判定を実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,73 @@
 #include <Novice.h>
+#include <math.h>
+
+#include <imgui.h>
+
+#define p_vi 3.14f;
 
 const char kWindowTitle[] = "GC1C_02_アリマ_ナオト";
+
+struct Vector2 {
+	float x;
+	float y;
+};
+
+struct Player {
+	Vector2 pos;
+	float newPosY;
+	float speed;
+	float radius;
+	unsigned int color;
+};
+
+struct FireBar {
+	Vector2 pos;
+	unsigned int color;
+};
+
+struct Circle {
+	Vector2 pos;
+	float newPosY;
+	unsigned int color;
+};
+
+void Move(Player& player,char *keys) {
+
+	if (keys[DIK_RIGHT]) {
+		player.pos.x += player.speed;
+	}
+
+	if (keys[DIK_LEFT]) {
+		player.pos.x -= player.speed;
+	}
+
+	if (keys[DIK_UP]) {
+		player.pos.y += player.speed;
+	}
+
+	if (keys[DIK_DOWN]) {
+		player.pos.y -= player.speed;
+	}
+}
+
+bool IsCollision(Player &player,Circle circle[5]) {
+
+	for (int i = 0; i < 5; i++) {
+
+		float dx = circle[i].pos.x - player.pos.x;
+		float dy = circle[i].pos.y - player.pos.y;
+
+		float distance = sqrtf(powf(dx, 2) + powf(dy, 2));
+
+		if (distance < 30 + 50) {
+			return true;
+			break;
+		}
+	}
+
+	return false;
+
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +78,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	Player player = {
+		.pos = { 200,50 },
+		.speed = { 5 },
+		.radius = { 50 },
+		.color = WHITE
+	};
+
+	FireBar fireBar = {
+		.pos{600,200},
+		.color = RED
+	};
+
+	const int Max = 5;
+
+	Circle circle[Max];
+	for (int i = 0; i < Max; i++) {
+		circle[i].pos = { 0,0 };
+		circle[i].color = RED;
+	}
+
+	float kRadius = 30;
+
+	float theta = 0.0f;
+
+	bool isFireBar = false;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,6 +118,37 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Move(player, keys);
+
+		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+			isFireBar = true;
+		}
+
+		if (isFireBar) {
+			theta += 1.0f / 90.0f * p_vi;
+		}
+
+		for (int i = 0; i < Max; i++) {
+			float localX = 2.0f * kRadius * i;
+			circle[i].pos.x = localX * cosf(theta) + fireBar.pos.x;
+			circle[i].pos.y = localX * sinf(theta) + fireBar.pos.y;
+		}
+
+		if (IsCollision(player, circle)) {
+			player.color = BLUE;
+		}
+		else {
+			player.color = WHITE;
+		}
+
+		for (int i = 0; i < Max; i++) {
+			circle[i].newPosY = (circle[i].pos.y - 500) * -1;
+		}
+
+		player.newPosY = (player.pos.y - 500) * -1;
+
+		
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,7 +156,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-	
+
+		for (int i = 0; i < Max;i++) {
+			Novice::DrawEllipse((int)circle[i].pos.x, (int)circle[i].newPosY, (int)kRadius, (int)kRadius, theta, circle[i].color, kFillModeSolid);
+		}
+
+		Novice::DrawEllipse((int)player.pos.x, (int)player.newPosY, (int)player.radius, (int)player.radius, 0.0f, player.color, kFillModeSolid);
+
 		///
 		/// ↑描画処理ここまで
 		///
